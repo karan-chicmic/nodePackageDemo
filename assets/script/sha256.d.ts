290200b22d7e83c1027e9fc6477a409b6c651514
Abhishek Rawat <abhishek.rawat@chicmic.co.in>
2024-04-29 12:28:08 +0530

    jshashes working

--- assets/script/sha256.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "js-sha256" {
-    import sha256 from "js-sha256";
-    export default sha256;
-}
--- assets/script/shaScript.ts
+++ assets/script/shaScript.ts
@@ -1,20 +1,27 @@
 import { _decorator, Component, Label, Node } from "cc";
-import sha256 from "js-sha256";
+
+import hash from "jshashes";
 const { ccclass, property } = _decorator;
 
 @ccclass("shaScript")
 export class shaScript extends Component {
     @property({ type: Label })
     label: Label = null;
-    start() {}
+    start() {
+        var hashString = new hash.SHA256().hex("Karan Kalra");
+        console.log(hashString);
+
+        // var SHA256 = new Hashes.SHA256();
+        // console.log(SHA256.hex("Karan Kalra"));
+    }
 
     update(deltaTime: number) {}
 
     onClick() {
-        console.log("onClick FUnction called!");
-        var hash = sha256.create();
-        // hash.update("Message to hash");
-        hash.update(this.label.string);
-        this.label.string = hash.hex();
+        // console.log("onClick FUnction called!");
+        // var hash = sha256.create();
+        // // hash.update("Message to hash");
+        // hash.update(this.label.string);
+        // this.label.string = hash.hex();
     }
 }
